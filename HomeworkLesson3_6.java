13400b8e14ff2ab80e5288e6303c8cc0b5c0a407
Egorvasil <21052009vas@gmail.com>
2022-06-14 14:51:54 +0300

    Add homework for lesson3

---
+++ HomeworkLesson3_6.java
@@ -0,0 +1,13 @@
+public class HomeworkLesson3_6 {
+    public static void main(String[] args) {
+       String[] matrix = {};
+       String[][] names = {
+               {"0" , "2", "0"},
+               {"4" , "0", "6"},
+               {"0" , "8", "0"}
+        };
+       System.out.println(names[0][0] + " " + names[0][1] + " " + names[0][2]);
+       System.out.println(names[1][0] + " " + names[1][1] + " " + names[1][2]);
+       System.out.println(names[2][0] + " " + names[2][1] + " " + names[2][2]);
+    }
+}
