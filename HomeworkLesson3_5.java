c463ec9dd6942dda8828213208245ad458dd1163
Egorvasil <21052009vas@gmail.com>
2022-06-21 11:14:14 +0300

    Add homework for lesson3

---
+++ HomeworkLesson3_5.java
@@ -0,0 +1,8 @@
+public class HomeworkLesson3_5 {
+    public static void main(String[] args) {
+        String s = "*";
+        for (int i = 0; i < 10 ; i++) {
+            System.out.println( s+="*");
+        }
+    }
+}
