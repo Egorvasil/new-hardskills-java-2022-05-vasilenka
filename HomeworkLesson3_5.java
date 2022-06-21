5672223274d6adfa16d50ef8f6713fce7413e8a7
Egorvasil <106623711+Egorvasil@users.noreply.github.com>
2022-06-21 10:52:37 +0300

    Delete HomeworkLesson3_5.java

--- HomeworkLesson3_5.java
+++
@@ -1,8 +0,0 @@
-public class HomeworkLesson3_5 {
-    public static void main(String[] args) {
-        String s = "";
-        for (int i = 1; i < 10; i++) {
-            System.out.println(s += "*");
-        }
-    }
-}
