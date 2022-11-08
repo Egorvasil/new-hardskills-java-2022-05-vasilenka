26c41cff4d5878d6732f358db6e13a63beecf924
Egorvasil <21052009vas@gmail.com>
2022-11-08 22:20:29 +0300

    delete not correct project

--- visit_psychologist/src/main/java/by/vasilenka/visit_psychologist/repositories/VisitPsychologistRepository.java
+++
@@ -1,9 +0,0 @@
-package by.vasilenka.visit_psychologist.repositories;
-
-import by.vasilenka.visit_psychologist.entities.VisitPsychologist;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface VisitPsychologistRepository extends JpaRepository<VisitPsychologist, Long> {
-
-}
-
